8fad55aa36881b55e0b38d1e542c77d2fa96b671
Brad Czerniak <ao5357@gmail.com>
2014-07-12 11:33:48 -0400

    Order services, add a pre-footer, style it

--- sites/all/themes/compro_tao/templates/page.tpl.php
+++ sites/all/themes/compro_tao/templates/page.tpl.php
@@ -2,5 +2,6 @@
 <?php if ($page['nav']) print render($page['nav']) ?>
 <?php if ($page['content_top']) print render($page['content_top']) ?>
 <?php if ($page['content']) print render($page['content']) ?>
-<?php if ($page['content_bottom']) print render($page['content_bottom']) ?>
+<?php if ($page['content_bottom']) print render($page['content_bottom'])?>
+<?php if ($page['pre_footer']) print render($page['pre_footer']) ?>
 <?php if ($page['footer']) print render($page['footer']) ?>
